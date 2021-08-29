6e2687f71ef4b41d08bcbbbbc2ca4b6d05c29d91
MFALLAM <mohammed.fathi.allam@gmail.com>
2021-08-30 00:11:02 +0300

    initial load

---
+++ app/src/main/java/com/example/uicalculator/calc_buttons.java
@@ -0,0 +1,14 @@
+package com.example.uicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calc_buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_buttons);
+    }
+}
---
+++ app/src/main/java/com/example/uicalculator/calc_result.java
@@ -0,0 +1,14 @@
+package com.example.uicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calc_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_result);
+    }
+}
